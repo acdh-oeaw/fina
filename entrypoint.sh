069011f495dc7d6bda9dd4aaf41ae651b0dd2d4f
ACDH-CH <105779568+acdh-ch@users.noreply.github.com>
2024-04-03 08:12:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,12 @@ export PATH="/app/.heroku/php/bin:${PATH}"
 cd /app/
 composer update
 php maintenance/update.php
-# php extensions/SemanticMediaWiki/maintenance/updateEntityCollation.php 
+php extensions/SemanticMediaWiki/maintenance/updateEntityCollation.php 
 
 
 # php extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php  --startOver
 # php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks —indexOnSkip 
 # php maintenance/runJobs.php
-
-sed -i -r -e 's|max_execution_time = 30|max_execution_time = 300|g' /app/.heroku/php/etc/php/php.ini
 
 # Start web server
 exec heroku-php-apache2 -F fpm_custom.conf
