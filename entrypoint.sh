06c38d9effa1d2edbe84c525875a9fd868533439
ACDH-CH <105779568+acdh-ch@users.noreply.github.com>
2024-07-24 09:04:28 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ php maintenance/update.php
 # php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks —indexOnSkip 
 # php maintenance/runJobs.php
 
-# Start web server
+## Start web server
 exec heroku-php-apache2 -F fpm_custom.conf
 # exec heroku-php-nginx
